dc91210718d6b7575e88dee4fba58fc0aad41010
Janie-Marie <48127595+kuskarina@users.noreply.github.com>
2025-01-27 16:44:04 -0600

    Update rps.py
    added exit feature and fixed uppercase/lowercase bug

--- TestPython/rps.py
+++ TestPython/rps.py
@@ -1,4 +1,5 @@
 from random import randint
+import os
 
 #create a list of play options
 t = ["Rock", "Paper", "Scissors"]
@@ -8,28 +9,35 @@ computer = t[randint(0,2)]
 
 #set player to false
 player = False
+os.system('clear')
+
 
 while player == False:
 #set player to True
-	player = input("Rock, Paper, Scissors?")
-	if player == computer:
+	player = input("Rock, Paper, Scissors? 'Exit' to quit!    ")
+	if player == computer or player == str.lower(computer):
 		print("Tie!")
-	elif player == "Rock":
+	elif player == "Rock" or player == "rock":
 		if computer == "Paper":
 			print("You loose!", computer, "covers", player)
 		else:
 			print("You win!", player, "smashes", computer)
-	elif player == "Paper":
+	elif player == "Paper" or player == "paper":
 		if computer == "Scissors":
 			print("You loose!", computer, "cuts", player)
 		else:
 			print("You win!", player, "covers", computer)
-	elif player == "Scissors":
+	elif player == "Scissors" or player == "scissors":
 		if computer == "Rock":
 			print("You loose...", computer, "smashes", player)
 		else:
 			print("You win!", player, "cuts", computer)
+	elif player == "Exit" or player == "exit":
+		print("Okay, thanks for playing!")
+		quit()
+		break
 	else:
 		print("That's not a valid play. Check your spelling!")
+		computer = t[randint(0,2)]
 	player = False
 	computer = t[randint(0,2)]
